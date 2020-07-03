c83ff270a8e74f981f9090076b55f00e824b2b9f
Ronny Jaramillo <ronny_jaramillo123@hotmail.es>
2020-07-03 16:02:35 -0500

    Suma de números validada.

--- 01-http/introduccion/src/app.module.ts
+++ 01-http/introduccion/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {HttpJuegoModule} from "./http/http-juego.module";
 
 @Module({
   imports: [
       //Aquí se incluyen otros módulos
+      HttpJuegoModule
   ],
   controllers: [AppController],//Aquí se incluyen los controladores
   providers: [AppService], //Aquí se incluyen los servicios APP MODULE
