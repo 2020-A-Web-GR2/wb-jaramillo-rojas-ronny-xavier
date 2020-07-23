6956a957d70aa66760f22ddd812fd6de020a2710
Ronny Jaramillo <ronny_jaramillo123@hotmail.es>
2020-07-23 16:12:52 -0500

    Inicio RESTful

--- 01-http/introduccion/src/app.module.ts
+++ 01-http/introduccion/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {HttpJuegoModule} from "./http/http-juego.module";
 import {HttpCalculadoraModule} from "./http/http-calculadora.module";
+import {UsuarioModule} from "./usuario/usuario.module";
 
 @Module({
   imports: [
       //Aquí se incluyen otros módulos
       HttpJuegoModule,
-      HttpCalculadoraModule
+      HttpCalculadoraModule,
+      UsuarioModule
   ],
   controllers: [AppController],//Aquí se incluyen los controladores
   providers: [AppService], //Aquí se incluyen los servicios APP MODULE
