dc7fbcddf553fd6c6c97cd624b1ff54dc85a1a87
Ronny Jaramillo <ronny_jaramillo123@hotmail.es>
2020-07-26 19:46:35 -0500

    Hasta mysql.

--- 01-http/introduccion/src/app.module.ts
+++ 01-http/introduccion/src/app.module.ts
@@ -4,13 +4,29 @@ import { AppService } from './app.service';
 import {HttpJuegoModule} from "./http/http-juego.module";
 import {HttpCalculadoraModule} from "./http/http-calculadora.module";
 import {UsuarioModule} from "./usuario/usuario.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
 @Module({
   imports: [
       //Aquí se incluyen otros módulos
       HttpJuegoModule,
       HttpCalculadoraModule,
-      UsuarioModule
+      UsuarioModule,
+      TypeOrmModule.forRoot({
+          name: 'default', //Nombre conexión
+          type: 'mysql', //mysql postgres, etc
+          host: 'localhost', //ip
+          port: 3306, //puerto
+          username: 'root', //user
+          password: '', //passwd
+          database: 'prueba', //Base de Datos
+          entities: [  // todas las entidades
+            UsuarioEntity
+          ],
+          synchronize: true, //Actualiza el esquema de la base de datos
+          dropSchema: false, // Eliminar Datos y Esquema de base de datos
+      })
   ],
   controllers: [AppController],//Aquí se incluyen los controladores
   providers: [AppService], //Aquí se incluyen los servicios APP MODULE
